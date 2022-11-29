95ac4d4e9d13ebd9f10a8201f3878ab2bfe82bb8
Kimi <porthan.kimi@gmail.com>
2022-11-29 17:23:03 +0200

    new desing

--- frontend/src/components/CustomCard.tsx
+++ frontend/src/components/CustomCard.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+
 export const CustomCard = ({
   name,
   ...props
@@ -5,17 +8,24 @@ export const CustomCard = ({
   name: string;
 }) => {
 
+  const [isCollapsed, setIsCollapsed] = useState(true)
 
+const cardClickHandler = () => {
+  setIsCollapsed(!isCollapsed)
+}
 
   return (
-    <div className="card">
+    <div className="card" onClick={cardClickHandler}>
       <div className="top">
         <h3>{name}</h3>
+        <span className="icon">
+          <FiChevronDown />
+        </span>
       </div>
-      <div className="bottom">
-        <p>Address</p>
-        <p>journeys from</p>
-        <p>journey to</p>
+      <div className="bottom" id={isCollapsed ? 'collapsed' : ''}>
+        <p>Address: Osoite 123</p>
+        <p>Journeys from this station: 122</p>
+        <p>Journeys to this station: 344</p>
       </div>
     </div>
   );
--- frontend/src/views/Stations.tsx
+++ frontend/src/views/Stations.tsx
@@ -5,25 +5,12 @@ export const Stations = (): JSX.Element => {
 
 
     return(
-        <div className="stations">
-
-           <CustomCard name="Station name"></CustomCard>
-           <CustomCard name="Station name 2"></CustomCard>
-           <CustomCard name="Station name 3"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-           <CustomCard name="Station name 4"></CustomCard>
-
-
-
-
-
-
-
+        <div>
+            <div className="stations">
+                <CustomCard name="Station name 1"></CustomCard>
+                <CustomCard name="Station name 2"></CustomCard>
+                <CustomCard name="Station name 3"></CustomCard>
+            </div>
         </div>
     )
 }
