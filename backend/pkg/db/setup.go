490bd2e197581eb6934e50a30577ebfc63a47dfc
Kimi <porthan.kimi@gmail.com>
2022-12-03 13:33:48 +0200

    remove dotsql

--- backend/pkg/db/setup.go
+++ backend/pkg/db/setup.go
@@ -2,35 +2,9 @@ package db
 
 import (
 	"log"
-	"os"
-
-	"github.com/qustavo/dotsql"
 )
 
-var DOT *dotsql.DotSql
-
-func getQueryFile() *dotsql.DotSql {
-	// Queries in separete file for ease of maintenance.
-	// get enviroment, because when testing, path changes.
-	env := os.Getenv("ENVIROMENT")
-	var path string
-	if env == "dev" {
-		path = "pkg/db/queries.sql"
-	} else {
-		path = "../db/queries.sql"
-	}
-
-	dot, err := dotsql.LoadFromFile(path)
-	if err != nil {
-		log.Fatal("Cannot load queries.sql")
-	}
-
-	return dot
-}
-
 func CreateTables() {
-	DOT = getQueryFile()
-
 	_, err := DB.Exec(createJourneyTableQuery)
 	if err != nil {
 		log.Fatal("error when creating journey table. Error: ", err)
