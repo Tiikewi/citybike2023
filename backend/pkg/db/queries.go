bcd1f26c43beae18a2c03ee19c0416c2b9cf84cb
Kimi <porthan.kimi@gmail.com>
2022-12-03 13:32:25 +0200

    move table creations

--- backend/pkg/db/queries.go
+++ backend/pkg/db/queries.go
@@ -1,5 +1,38 @@
 package db
 
+const createJourneyTableQuery = `
+CREATE TABLE IF NOT EXISTS journey (
+	id                     INT auto_increment NOT NULL PRIMARY KEY,
+	departure_time         DATETIME NOT NULL,
+	return_time            DATETIME NOT NULL,
+    departure_station_id   INT NOT NULL,
+    return_station_id      INT NOT NULL,
+    distance               INT NOT NULL,
+    duration               INT NOT NULL,
+
+    FOREIGN KEY(departure_station_id)
+    REFERENCES station(id),
+    FOREIGN KEY(return_station_id)
+    REFERENCES station(id)
+)`
+
+const createStationTableQuery = `
+CREATE TABLE IF NOT EXISTS station (
+	fid                    INT NOT NULL,
+    id                     INT NOT NULL PRIMARY KEY,
+    station_name_finnish   VARCHAR(100) NOT NULL,
+    station_name_swedish   VARCHAR(100) NOT NULL,
+    station_name_english   VARCHAR(100) NOT NULL,
+    address_finnish        VARCHAR(100) NOT NULL,
+    address_swedish        VARCHAR(100) NOT NULL,
+    city_name_finnish      VARCHAR(100) NOT NULL,
+    city_name_swedish      VARCHAR(100) NOT NULL,
+    operator               VARCHAR(100),
+    capacity               INT NOT NULL,
+    x_coordinate           FLOAT NOT NULL,
+    y_coordinate           FLOAT NOT NULL
+)`
+
 const getJourneysQuery = `SELECT j.*, s1.station_name_finnish AS departure_station, 
 	s2.station_name_finnish AS return_station FROM journey j
 	LEFT OUTER JOIN station s1 ON j.departure_station_id = s1.id
--- backend/pkg/db/setup.go
+++ backend/pkg/db/setup.go
@@ -47,11 +47,11 @@ func getQueryFile() *dotsql.DotSql {
 func CreateTables() {
 	DOT = getQueryFile()
 
-	_, err := DOT.Exec(DB, "create-journey-table")
+	_, err := DB.Exec(createJourneyTableQuery)
 	if err != nil {
 		log.Fatal("error when creating journey table. Error: ", err)
 	}
-	_, err = DOT.Exec(DB, "create-station-table")
+	_, err = DB.Exec(createStationTableQuery)
 	if err != nil {
 		log.Fatal("error when creating station table. Error: ", err)
 	}
