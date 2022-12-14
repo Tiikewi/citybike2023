9e9017a8b73831b1bc8d3fe52336349a389748b7
Kimi <porthan.kimi@gmail.com>
2022-12-14 14:34:23 +0200

    add content to landing page

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -3,13 +3,21 @@ import '../styles/home.css'
 export const Home = (): JSX.Element => {
     return(
       <div className='body'>
-        <h1>This is homepage</h1>
-          <p>Some useful information here.</p>
+        <h1>Citybike 2023</h1>
+        <p>This is the pre-assignment for Solita Dev Academy 2023.</p>
+          <p>Citybike is app for displaying data of journeys made with city bikes in Helsinki Capital area.</p>
+          <p>Going to implement some improvements during application period, depending amount of time I have available.</p>
+          <br></br>
+
+          <h3>TODOS</h3>
           <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
+            <li>Really improve and implement tests</li>
+            <li>Make UI responsive for various screen sizes</li>
+            <li>UI element for creating journeys/stations</li>
+            <li>Improve CSV reading and validation and perhaps add ability to add csv files to db from client.</li>
           </ul>
         </div>
     )
   }
+
+  
