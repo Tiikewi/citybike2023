f8610f5371250bc06d83838adbd0d9980e0431b2
Kimi <porthan.kimi@gmail.com>
2022-12-16 17:10:51 +0200

    add correct form fields

--- frontend/src/components/StationModal.tsx
+++ frontend/src/components/StationModal.tsx
@@ -3,10 +3,26 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
+type newStation = {
+    id: number,
+    name: string,
+    address: string,
+    city: string,
+    coordinates: {x: number, y: number},
+    capacity?: number,
+    operator?: string
+}
+let newSt: newStation;
+
 export const StationModal = () => {
   const [show, setShow] = useState(false);
+  const [station, setStation] = useState<newStation>(newSt);
 
   const handleClose = () => setShow(false);
+  const handleAdd = () => {
+    
+      setShow(false);
+  }
   const handleShow = () => setShow(true);
 
   return (
@@ -21,20 +37,49 @@ export const StationModal = () => {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
+            <Form.Group className="mb-3" controlId="stationInfo">
+              <Form.Label>Name</Form.Label>
               <Form.Control
-                type="email"
-                placeholder="name@example.com"
+                type="text"
+                placeholder="Station name"
                 autoFocus
               />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Station address"
+              />
+            <Form.Label>ID</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Station ID"
+                />
+            <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City name"
+                />
+            <Form.Label>Capacity</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Stations capacity"
+                />
+            <Form.Label>Operator</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Operator"
+                />
+            <Form.Label>Coordinates</Form.Label>
+              <Form.Control
+                type="number"
+                step="0.01"
+                placeholder="latitude"
+                />
+              <Form.Control
+                type="number"
+                step="0.01"
+                placeholder="longitude"
+                />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -42,7 +87,7 @@ export const StationModal = () => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleAdd}>
             Add
           </Button>
         </Modal.Footer>
